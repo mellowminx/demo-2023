b2c242ae03aa57124cf71c3d46b988bead1d1c98
mellowminx <hello@mellowminx.com>
2023-10-08 15:09:26 +0800

    polygon demo parameters

--- project/Icon.gd
+++ project/Icon.gd
@@ -6,13 +6,17 @@ extends Sprite
 
 func _ready():
 	_create_collision_polygon()
+	get_parent().position = Vector2(100,100)
 
 func _create_collision_polygon():
 		var bm = BitMap.new()
 		bm.create_from_image_alpha(texture.get_data())
 		# in the original script, it was Rect2(position.x, position.y ...)
 		var rect = Rect2(0, 0, texture.get_width(), texture.get_height())
-		var my_array = bm.opaque_to_polygons(rect, 0.0001)
+		# change (rect, 2) for more or less precision
+		# for ex. (rect, 5) will have the polygon points spaced apart more
+		# (rect, 0.0001) will have points spaced very close together for a precise outline
+		var my_array = bm.opaque_to_polygons(rect, 2)
 		# optional - check if opaque_to_polygons() was able to get data
 #		print(my_array)
 		var my_polygon = Polygon2D.new()
